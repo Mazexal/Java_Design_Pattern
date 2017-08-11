5b1fcd2cc694156cb9724842988548be01047a54
Mazexal <1023520044@qq.com>
2017-08-11 23:22:26 +0800

    通讯模式

---
+++ src/main/java/通讯模式/StudentVO.java
@@ -0,0 +1,30 @@
+package 通讯模式;
+
+/**
+ * Created by 10235 on 2017/8/11.
+ */
+public class StudentVO {
+    private String name;
+    private int rollNo;
+
+    StudentVO(String name, int rollNo){
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+}
