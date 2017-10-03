328ef5218a905e7a31ee8b2dcfd7a9c0321f4b67
Mazexal <1023520044@qq.com>
2017-10-03 23:32:36 +0800

    观察者模式

--- src/main/java/命令模式/CommandPatternDemo.java
+++ src/main/java/命令模式/CommandPatternDemo.java
@@ -11,6 +11,7 @@ public class CommandPatternDemo {
         Broker broker = new Broker();
         broker.takeOrder(buyStockOrder);
         broker.takeOrder(sellStockOrder);
+        
         broker.placeOrders();
     }
 }
